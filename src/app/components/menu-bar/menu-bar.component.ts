6bc5bfe6194c342da0e9d08b93b450fd77c4568f
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-09-21 17:36:54 -0300

    Instalação de ícones das redes sociais

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faLinkedin,
+  faInstagram,
+  faGithub,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css'],
 })
 export class MenuBarComponent implements OnInit {
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
+  faGithub = faGithub;
+  faTwitter = faTwitter;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
